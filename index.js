97c75274519826a0c9927c3ea1249386b2613ea9
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-15 03:15:50 -0600

    starting functionality of war

---
+++ index.js
@@ -0,0 +1,55 @@
+
+let deckId
+
+/* handling a new deck */
+
+function handleClick(){
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      deckId = data.deck_id
+    }) 
+}
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+// Draw
+
+function drawCard() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+document.getElementById("draw").addEventListener("click", drawCard)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+// * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+// * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+// * that you'll see in the deck of cards API docs.
