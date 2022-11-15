9613bdb297878ad78d33798ebe5f031a5240beb4
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-15 04:18:44 -0600

    CSS complete

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 function drawCard() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    document.getElementById("cards").innerHTML = `
+    <img src= ${data.cards[0].image} />
+    <img src= ${data.cards[1].image} />
+    `
+  })
+ 
 }
 document.getElementById("draw").addEventListener("click", drawCard)
+
+//DisplayCards
 
 
 
